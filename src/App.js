71b734d2f9a76f632d31c61a4468a28dccdb0465
zubimirza <zubairzhni@gmail.com>
2023-07-30 04:52:23 +0500

    My todo-app Complete

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import {useState} from "react"
+import List from "./List";
+function App() {
+  const [currentItem , setCurrentItem] = useState(null);
+  const [itemList , updateItemList] = useState([]);
+  const onChangeHandler = e => {
+  // console.log('curent Value',e.target.value)
+  setCurrentItem(e.target.value)
+}
+const addItemToList=()=>{
+updateItemList([...itemList, {item : currentItem , key : Date.now() }])
+setCurrentItem("")
+// console.log(itemList)
+}
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div className='wrapper'>
+        <div className='input-wrapper' >
+          <input value={currentItem} onChange={onChangeHandler} placeholder="Enter Todo......" />
+          <button onClick={addItemToList}   className="btn2" >Add</button>
+        </div>
+        <List itemList={itemList} updateItemList={updateItemList} />
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
+ 
---
+++ src/List.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./List.css";
+function List(props) {
+    const deleteItemFromList = key => {
+      const newList =props.itemList.filter(itemObj=>{
+        return itemObj.key !== key;
+      });
+      props.updateItemList(newList);
+    }; 
+
+  return (
+    <div className='text-center' >
+{props.itemList.map(itemObj =>{
+  return (
+  <div className='item' >
+     <p>{itemObj.item}</p>
+      <button onClick={() => deleteItemFromList(itemObj.key)} className='btn1' >Del </button>
+  </div>
+  );
+})}
+    </div>
+
+  )
+}
+
+export default List ;
